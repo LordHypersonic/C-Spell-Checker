8f900d6871cdc752475b16e9f0a265b6209de9ef
Lord_Hypersonic <jatin.bal16@gmail.com>
2021-09-18 16:12:14 +0530

    Update spell_checker.cpp

--- Spell Checker/spell_checker.cpp
+++ Spell Checker/spell_checker.cpp
@@ -225,10 +225,9 @@ int mixedExtraMissing (string input)
 
 int main()
 {
-    string input,line;
-    int len,flen,correct=0;
-    while (1) {
-        correct = 0;
+    while (1) {  
+        string input,line;
+        int len,flen,correct=0;
         cout<<"Enter the word: "; getline(cin,input);
         len = input.length();
         for (int i=0; i < len; i++)
